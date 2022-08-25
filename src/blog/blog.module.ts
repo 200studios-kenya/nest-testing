fbcc9c57559b6da863fdb89109a46c469df98172
Joel Wekesa <67539766+JoelWekesa@users.noreply.github.com>
2022-08-25 10:45:17 +0300

    Changed Schema

--- src/blog/blog.module.ts
+++ src/blog/blog.module.ts
@@ -1,9 +1,14 @@
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { BlogService } from './blog.service';
 import { BlogController } from './blog.controller';
+import BlogSchema, { Blog } from './blog.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Blog.name, schema: BlogSchema }]),
+  ],
   controllers: [BlogController],
-  providers: [BlogService]
+  providers: [BlogService],
 })
 export class BlogModule {}
